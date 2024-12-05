918e6834b4f51044214bea1054f770bf91ab0e85
Ricardo Cortes <rcr.786@gmail.com>
2024-12-04 22:07:55 -0600

    fixed product images path and new build

--- public/build/assets/ApplicationLogo-B7OagKZz.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-e5j0Ovg5.js";function i(o){return r.jsx("img",{...o,src:"/images/GarageSale.png"})}export{i as A};
---
+++ public/build/assets/ApplicationLogo-DcbZ8mxZ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-D6DgRNtV.js";function i(o){return r.jsx("img",{...o,src:"/images/GarageSale.png"})}export{i as A};
