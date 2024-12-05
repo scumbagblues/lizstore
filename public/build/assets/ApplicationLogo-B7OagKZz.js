d27d8ad0855d0c622b5ee88dbdfca423dc55bfb0
Ricardo Cortes <rcr.786@gmail.com>
2024-12-04 21:54:23 -0600

    Added daisy ui

---
+++ public/build/assets/ApplicationLogo-B7OagKZz.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-e5j0Ovg5.js";function i(o){return r.jsx("img",{...o,src:"/images/GarageSale.png"})}export{i as A};
--- public/build/assets/ApplicationLogo-DSV8pQqT.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-Xgs67kNT.js";function i(o){return r.jsx("img",{...o,src:"/images/GarageSale.png"})}export{i as A};
