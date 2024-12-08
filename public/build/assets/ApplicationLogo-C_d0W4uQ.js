c04d12ca91273d1dbcd5c04a25d82140dc42a4a3
Ricardo Cortes <rcr.786@gmail.com>
2024-12-07 23:55:42 -0600

    Added link to return home in garage sale image- new build

---
+++ public/build/assets/ApplicationLogo-C_d0W4uQ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-BwIOfWPw.js";function i(o){return r.jsx("img",{...o,src:"/images/GarageSale.png"})}export{i as A};
--- public/build/assets/ApplicationLogo-CfF_ZaXS.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BATqaq7h.js";function i(o){return r.jsx("img",{...o,src:"/images/GarageSale.png"})}export{i as A};
