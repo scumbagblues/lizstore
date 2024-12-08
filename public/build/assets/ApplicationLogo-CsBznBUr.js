89e0dcb2225d69d4aae65457352d76bbe642260c
Ricardo Cortes <rcr.786@gmail.com>
2024-12-07 23:04:59 -0600

    Added image default when no images. Fixed focus in create product. New build

---
+++ public/build/assets/ApplicationLogo-CsBznBUr.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-l7tchI1i.js";function i(o){return r.jsx("img",{...o,src:"/images/GarageSale.png"})}export{i as A};
--- public/build/assets/ApplicationLogo-DcbZ8mxZ.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-D6DgRNtV.js";function i(o){return r.jsx("img",{...o,src:"/images/GarageSale.png"})}export{i as A};
