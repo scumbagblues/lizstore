53d6641e91b3d78b05bc2cfb9d7b5a322a8f987f
Ricardo Cortes <rcr.786@gmail.com>
2024-12-08 12:42:09 -0600

    Added flag to publish products and other minor styling issues- new build

--- public/build/assets/ApplicationLogo-C_d0W4uQ.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BwIOfWPw.js";function i(o){return r.jsx("img",{...o,src:"/images/GarageSale.png"})}export{i as A};
---
+++ public/build/assets/Checkbox-ByNinvSE.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-D89be0My.js";function s({className:r="",...o}){return a.jsx("input",{...o,type:"checkbox",className:"rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800 "+r})}export{s as C};
