9923237d8aa3c1850d273d8f0045efd16f677807
Ricardo Cortes <rcr.786@gmail.com>
2025-01-20 17:25:50 -0600

    Changed phone number build

---
+++ public/build/assets/ApplicationLogo-BlO9XoYy.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-DBM5V8L0.js";function i(o){return r.jsx("img",{...o,src:"/images/GarageSale.png"})}export{i as A};
--- public/build/assets/ApplicationLogo-asbNJCni.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CGCcFwB2.js";function i(o){return r.jsx("img",{...o,src:"/images/GarageSale.png"})}export{i as A};
